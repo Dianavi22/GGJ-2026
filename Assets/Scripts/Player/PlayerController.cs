afa701fc1ea4c2cb65d4327b1e14907d9bc66174
Martin Berthier <martin.berthier@obspm.fr>
2026-01-31 17:56:38 +0100

    new player controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,34 +8,31 @@ public class PlayerController : MonoBehaviour
   [SerializeField] private float _interpolationSpeed;
 
   private Rigidbody _rigidbody;
-  private bool _lookingBehind;
 
   private void Awake() {
     _rigidbody = GetComponent<Rigidbody>();
   }
 
   private void Update() {
-    _lookingBehind = Input.GetAxisRaw("Jump") == 1;
+    if(Input.GetKeyDown(KeyCode.Space)) {
+      _rigidbody.MoveRotation(Quaternion.LookRotation(-transform.forward, Vector3.up));
+    }
   }
 
   private void FixedUpdate() {
-    Vector3 movementDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+    float hInput = Input.GetAxis("Horizontal");
+    float vInput = Input.GetAxis("Vertical");
 
-    Vector3 moveVect = _speed * Time.fixedDeltaTime * movementDir;
+    Vector3 moveVect = transform.forward * _speed * Time.fixedDeltaTime * vInput;
     _rigidbody.MovePosition(_rigidbody.position + moveVect);
 
-    if (movementDir != Vector3.zero) Rotate(movementDir);
-  }
-
-
-  private void Rotate(Vector3 direction) {
-    if(_lookingBehind) {
-      direction = -direction;
-    }
-
-    direction += transform.position;
-
-    Quaternion targetRotation = Quaternion.LookRotation(direction - transform.position);
-    transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _interpolationSpeed);
+    float rotAngle = hInput * _rotationSpeed * Time.fixedDeltaTime;
+    Quaternion qRot = Quaternion.AngleAxis(rotAngle, transform.up);
+    Quaternion qRotUpRight = Quaternion.FromToRotation(transform.up, Vector3.up);
+    Quaternion qOrientationUpRightTarget = qRotUpRight * _rigidbody.rotation;
+    Quaternion qNewUpRightOrientation = Quaternion.Slerp(_rigidbody.rotation, qOrientationUpRightTarget, Time.fixedDeltaTime * _interpolationSpeed);
+    _rigidbody.MoveRotation(qRot * qNewUpRightOrientation);
+    _rigidbody.velocity = Vector3.zero;
+    _rigidbody.angularVelocity = Vector3.zero;
   }
 }
